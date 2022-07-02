2a6d6963c764e37fa3c26b2d2a49ff598a973edc
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-02 11:51:48 -0500

    Modificacion al archivo de codewars, total amount of points

--- codewars/totalAmountOfPoints.js
+++ codewars/totalAmountOfPoints.js
@@ -16,3 +16,13 @@ const points = (games) => {
 };
 
 points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]);
+
+// Usando operador ternario
+// function points(games) {
+//   return games.reduce((output,current)=>{
+//     let x = parseInt(current[0]);
+//     let y = parseInt(current[2]);
+//     let value= x>y ? 3 : x===y ? 1 : 0;
+//     return output+value;
+//   },0)
+// }
