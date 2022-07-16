b180fe69e8c815b1a04951119e5f9537f4ebb728
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-16 11:34:49 -0500

    Se agregaron 2 ejercicios nuevos de codewars jijiji

---
+++ codewars/convert-boolean-to-string.js
@@ -0,0 +1,14 @@
+/* 
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+const booleanToString = (b) => {
+    console.log(b);
+    console.log(typeof(b));
+    toString(b);
+    console.log(b);
+    console.log(typeof(b));
+};
+booleanToString(true);
